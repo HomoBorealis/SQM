9fb1fdcaac2611e34959e03fe905f3d6020a1f30
HomoBorealis <issagramis@gmail.com>
2024-05-05 11:50:33 +0300

    Add files via upload

---
+++ Functions.ino
@@ -0,0 +1,485 @@
+//Начальный экран
+void startScreen()
+{
+  TFTscreen.stroke(0, 0, 255);
+  TFTscreen.noFill();
+TFTscreen.rect (108,48,19,32);//индикатор бататареи (потом добавить)
+ TFTscreen.fill(0,0,255);
+ TFTscreen.rect (112,52,11,24);
+TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.rect(0,0,48,48);
+TFTscreen.rect (48,0,79,48);
+
+TFTscreen.rect (0,48,22,16);//активные спутники
+TFTscreen.rect (0,64,22,16);//видимые спутники
+TFTscreen.rect(0,48,127,112);
+TFTscreen.rect(0,48,127,112);
+TFTscreen.rect(0,112,127,159);
+  }
+void ModeFunc()//функция прерывания для переключения режимов
+{
+  
+  if (millis() - debounce >= 100 && digitalRead(3)) {//эта вся фигня для устранения дребезга контактов(лень паять кондёры)
+    debounce = millis();
+Mode++;
+
+ if (Mode >3){
+    Mode=0;
+  }
+ // Serial.println(Mode);
+  //TFTscreen.background(0, 0, 0);
+}
+}
+  
+//функция выведения показаний SQM
+void DisplSQM()
+{
+  TFTscreen.stroke(0, 0, 255);
+  TFTscreen.setTextSize(2);//установим размер всего выводимого далее текста
+  TFTscreen.text("SQM\n", 70,2);//выведем люксы 
+String LuxString=String(mag);
+  //преобразовать измерение в символьный массив
+  LuxString.toCharArray(luxstring,6);
+  TFTscreen.text(luxstring,55,20);
+TFTscreen.setTextSize(1);
+TFTscreen.text("Mag/ArcSec^2\n", 52, 38);
+}
+
+//функция выведения метеоданных
+void DisplMeteo()
+{
+  TFTscreen.stroke(0,0,255);//установит цвет текста (красный)
+  TFTscreen.setTextSize(1);//установим размер всего выводимого далее текста
+  TFTscreen.text("Temperature:\n", 5,116);//выведем слово температура
+  String tempString=String(Temp);
+  //преобразовать измерение в символьный массив
+  tempString.toCharArray(tempstring,3);
+  TFTscreen.text(tempstring,80,116);//выведем температуру
+  TFTscreen.text("*C",97,116);
+TFTscreen.text("Pressure:\n", 5,131);//выведем слово давление
+  String presString=String(Pres);
+  //преобразовать измерение в символьный массив
+  presString.toCharArray(presstring,5);
+  TFTscreen.text(presstring,60,131);//выведем давление
+  TFTscreen.text("Pa",85,131);
+TFTscreen.text("Humidity:\n", 5,146);//выведем слово влажность
+  String humString=String(Hum);
+  //преобразовать измерение в символьный массив
+  humString.toCharArray(humstring,3);
+  TFTscreen.text(humstring,60,146);//выведем влажность (уточнить координату х)
+  TFTscreen.text("%",75,146);
+}
+
+//функция фазы луны и времени восхода
+void ReadMoon()
+{
+  //итог расчёта записывается в переменные phase и moon
+  old = day+month+moonNum;//для 24года лунное число = 18, каждый год+11, если больше 30 то 30 вычитается
+  if(old>30)
+  {old=old-30;}
+ /*
+  //время восхода = возраст луны *0,8 +6 время в часах, если больше 24, то 24 отнимаем
+  moon=old*0.8+6;
+  if(moon>24)
+  {
+    moon=moon-24;
+  }
+  */
+  
+}
+
+//функция отображения фазы луны и времени восхода (да, можно было вывести битовый массив как изображение, но я не ищу лёгких путей)
+void DisplMoon()
+{
+if (old >= 29&& old<=2){//новолуние
+  TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+}
+if (old >=3 && old <= 6){//молодая
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(255, 0, 0);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(0,0,0);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.circle(20,20,12);
+}
+if(old >=7 && old <=10){//первая четверть
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(0,0,0);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.rect(5,5,20,30);
+}
+if (old >=11 && old < 14)//прибывающая
+{
+TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(255,0,0);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.circle(27,20,12);
+TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+}
+if (old >= 14 && old <= 17)//полнолуние
+{
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(0,0,255);
+TFTscreen.circle(24,20,12);
+}
+if (old >=18 && old <=20)//убывающая
+{
+TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.circle(20,20,12);
+TFTscreen.noFill();
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+}
+if (old >=21 && old <=24)//последняя четверть
+{
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(0,0,0);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.rect(24,5,20,30);
+}
+if (old >=25 && old <=28)//Старая
+{
+TFTscreen.fill(0,0,255);
+TFTscreen.stroke(0, 0, 255);
+TFTscreen.circle(24,20,12);
+TFTscreen.fill(0,0,0);
+TFTscreen.stroke(0, 0, 0);
+TFTscreen.circle(28,20,12);
+}
+/*
+    String moonString=String(moon);//код вывода восхода Луны
+    moonString.toCharArray(moonstring,3);
+  TFTscreen.text(moonstring,10,38);
+  */
+}
+
+//функция выведения показаний GPS
+void DisplGPS()
+{
+  TFTscreen.stroke(0,0,255);
+TFTscreen.setTextSize(3);
+  if (hour < 10)//костыль из-за хреновой библиотеки
+  {
+    TFTscreen.setTextSize(3);
+     String hourString=String(hour);
+hourString.toCharArray(hourstring,3);
+  TFTscreen.text(hourstring,43,52);
+    TFTscreen.text("0", 25,52);
+  }
+   if (hour >=10)
+  {
+    TFTscreen.setTextSize(3);
+   String hourString=String(hour);
+hourString.toCharArray(hourstring,3);
+  TFTscreen.text(hourstring,25,52);
+  }
+TFTscreen.text(" : \n", 40,52);
+if (minute < 10)//тоже костыль из-за хреновой библиотеки
+  {
+    TFTscreen.setTextSize(3);
+    String minuteString=String(minute);
+minuteString.toCharArray(minutestring,3);
+  TFTscreen.text(minutestring,91,52);
+  TFTscreen.text("0", 73,52);
+  }
+  if (minute >=10)
+  {
+    TFTscreen.setTextSize(3);
+    String minuteString=String(minute);
+minuteString.toCharArray(minutestring,3);
+  TFTscreen.text(minutestring,73,52);
+  }
+if (day<10){ //иииии....ещё один костыль!
+  TFTscreen.setTextSize(1);
+  String dayString=String(day);
+dayString.toCharArray(daystring,3);
+TFTscreen.text("0", 38,75);
+  TFTscreen.text(daystring,44,75);
+}
+if (day>=10)
+{
+  TFTscreen.setTextSize(1);
+  String dayString=String(day);
+dayString.toCharArray(daystring,3);
+  TFTscreen.text(daystring,38,75);
+}
+TFTscreen.text(".\n", 48,75);
+if (month<10){ //да, код прямо скажу инвалид 1 группы...
+  TFTscreen.setTextSize(1);
+ String monthString=String(month);
+monthString.toCharArray(monthstring,3);
+  TFTscreen.text(monthstring,59,75);
+TFTscreen.text("0", 53,75);
+}
+if (month>=10)
+{
+  TFTscreen.setTextSize(1);
+String monthString=String(month);
+monthString.toCharArray(monthstring,3);
+  TFTscreen.text(monthstring,53,75);
+}
+String monthString=String(month);
+monthString.toCharArray(monthstring,3);
+  TFTscreen.text(monthstring,53,75);
+TFTscreen.text(".\n", 63,75);
+String yearString=String(year);
+yearString.toCharArray(yearstring,5);
+  TFTscreen.text(yearstring,68,75);
+
+TFTscreen.text("Latitude:\n", 5,85);// выведем широту
+  String latString=String(Lat);
+    latString.toCharArray(latstring,10);
+  TFTscreen.text(latstring,60,85); 
+ TFTscreen.text("Longitude:\n", 5,95);
+  String longString=String(Long);
+    longString.toCharArray(longstring,10);
+  TFTscreen.text(longstring,65,95);//выведем долготу
+ 
+ TFTscreen.text("Altitude:\n", 5,105);
+  String altString=String(Alt);//выведем высоту
+    altString.toCharArray(altstring,4);
+  TFTscreen.text(altstring,65,105);
+   
+     String satactString=String(SatAct);
+    satactString.toCharArray(satactstring,3);
+  TFTscreen.text(satactstring,5,53);//выведем активные спутники
+  String satvisString=String(SatVis);//выведем количество видимых спутников
+    satvisString.toCharArray(satvisstring,3);
+  TFTscreen.text(satvisstring,5,69);
+
+}
+
+
+//функция чтения показаний люксометра и вычисление MAG
+void ReadSQM() { 
+  String gainString = "Max gain";
+  delay(50);
+  tsl.setGain(TSL2591_GAIN_HIGH);// максимальное усиление люксометра
+  luminosity = tsl.getFullLuminosity();//проводим первое измерение
+  delay(50);
+  luminosity = tsl.getFullLuminosity(); //проводим второе для стабилизации показаний.
+  ir = luminosity >> 16;//вычисляем ИК составляющую
+  full = luminosity & 0xFFFF;//полный спектр
+  visible = full - ir;//вычисляем освещённость видимого спектра
+  if (visible== 0xFFFF||ir==0xFFFF) {//далее проверяем освещённость и снижаем усиление при высокой
+    gainString = "High gain";
+    tsl.setGain(TSL2591_GAIN_HIGH);
+    gainscale = HIGHSCALE;
+    luminosity = tsl.getFullLuminosity();
+    delay(50);
+    luminosity = tsl.getFullLuminosity();
+    ir = luminosity >> 16;
+    full = luminosity & 0xFFFF;
+    visible = full - ir;
+    if (visible == 0xFFFF || ir == 0xFFFF) {
+      gainString = "Med gain";
+      tsl.setGain(TSL2591_GAIN_MED);
+      gainscale = MEDSCALE;
+      luminosity = tsl.getFullLuminosity();
+      delay(50);
+      luminosity = tsl.getFullLuminosity();
+      ir = luminosity >> 16;
+      full = luminosity & 0xFFFF;
+      visible = full - ir;
+      if (visible == 0xFFFF || ir == 0xFFFF) {
+        gainString = "Low gain";
+        tsl.setGain(TSL2591_GAIN_LOW);
+        gainscale = LOWSCALE;
+        luminosity = tsl.getFullLuminosity();
+        delay(50);
+        luminosity = tsl.getFullLuminosity();
+        ir = luminosity >> 16;
+        full = luminosity & 0xFFFF;
+        visible = full - ir;
+
+      }
+    }
+  }
+  adjustedIR = (float)ir / gainscale;
+  adjustedVisible = (float)visible / gainscale;
+  mag = -1.085736205*log(.925925925 * pow(10,-5.)*adjustedVisible);//вычисляем значение темноты неба
+  if (isinf(mag)) {
+    mag = 25.0;
+  }
+  mag = mag + ReadEESqmCalOffset();//калибровка значения темноты неба (из EEPROM)
+  mag = float (int ( mag * 100 + 0.5) ) /100;
+       for (int a = 119 ; a >= 0 ; --a )
+{
+sqmArray[a] = sqmArray[a - 1];
+}
+sqmArray[0] = mag;
+}
+
+
+//Функция чтения метеоданных
+void ReadMeteo()
+{
+  Pres = bme.readPressure();
+    Temp = bme.readTemperature();
+    Hum =  bme.readHumidity();
+     Temp = Temp + TempCalOffset;  //с калибровкой
+     for (int a = 119 ; a >= 0 ; --a )
+{
+tempArray[a] = tempArray[a - 1];
+}
+tempArray[0] = Temp;
+    for (int a = 119 ; a >= 0 ; --a )
+{
+presArray[a] = presArray[a - 1];
+}
+presArray[0] = Pres;
+}
+
+//функция чтения показаний gps
+void ReadGPS()
+{
+gps.read();//  Читаем данные (чтение может занимать больше 1 секунды). Функции можно указать массив для получения данных о спутниках.
+
+     /* Время: */  hour=gps.Hours; minute=gps.minutes;
+     /* Дата:*/  day=gps.day; month=gps.month; year=gps.year;
+     /* Координаты (широта, долгота, высота): */  Lat=gps.latitude; Long=gps.longitude; Alt=gps.altitude;
+     /* Спутники (активные/наблюдаемые):          */ SatAct=gps.satellites[GPS_ACTIVE]; SatVis=gps.satellites[GPS_VISIBLE];
+    
+     if (year==24)//вычислять лунное число буду пока так
+     {
+      moonNum=18;
+     }
+     else
+     {
+      moonNum=((year-24)*11)+18;
+      if (moonNum>=30)
+      {
+        moonNum=moonNum-30;
+     }
+     }
+}
+void GraphTemp()//2 режим - вывод графика температуры
+{
+  int q;
+// Вывести текущую температуру
+TFTscreen.background(0, 0, 0); // Очистить экран, установив черный цвет фона
+TFTscreen.stroke(0, 0, 255); 
+TFTscreen.setTextSize(1);
+TFTscreen.text("Temperature:", 10, 5);
+String tempString=String(Temp);
+  //преобразовать измерение в символьный массив
+  tempString.toCharArray(tempstring,3);
+  TFTscreen.text("*C", 105, 5);
+TFTscreen.text(tempstring, 85, 5);
+// Нарисовать оси графика
+TFTscreen.setTextSize(1);
+TFTscreen.text("+35", 5, 20);
+TFTscreen.text("+30", 5, 30);
+TFTscreen.text("+25", 5, 40);
+TFTscreen.text("+20", 5, 50);
+TFTscreen.text("+15", 5, 60);
+TFTscreen.text("+10", 5, 70);
+TFTscreen.text("+5", 5, 80);
+TFTscreen.text("0", 5, 90);
+TFTscreen.text("-5", 5, 100);
+TFTscreen.text("-10", 5, 110);
+TFTscreen.text("-15", 5, 120);
+TFTscreen.text("-20", 5, 130);
+TFTscreen.text("-25", 5, 140);
+TFTscreen.text("-30", 5, 150);
+TFTscreen.line(25, 25, 25, 159);
+// Нарисовать график изменения температуры
+for (int a = 30 ; a < 127 ; a++)
+{
+// Преобразовать температуру в координату Y на экране ЖКИ
+q = (99 - (tempArray[a - 30] * 2));
+TFTscreen.point(a,q);
+}
+}
+
+void GraphSQM()
+{
+   int q;
+// Вывести текущую освещённость
+//TFTscreen.background(0, 0, 0); // Очистить экран, установив черный цвет фона
+TFTscreen.stroke(0, 0, 255); 
+TFTscreen.setTextSize(1);
+TFTscreen.text("SQM:", 3, 5);
+String LuxString=String(mag);
+  //преобразовать измерение в символьный массив
+  LuxString.toCharArray(luxstring,5);
+  TFTscreen.text("Mag/ArcSec^2\n", 57, 5);
+TFTscreen.text(luxstring, 27, 5);
+// Нарисовать оси графика
+TFTscreen.setTextSize(1);
+TFTscreen.text("24", 5, 20);
+TFTscreen.text("23.5", 5, 30);
+TFTscreen.text("23", 5, 40);
+TFTscreen.text("22.5", 5, 50);
+TFTscreen.text("22", 5, 60);
+TFTscreen.text("21.5", 5, 70);
+TFTscreen.text("21", 5, 80);
+TFTscreen.text("20.5", 5, 90);
+TFTscreen.text("20", 5, 100);
+TFTscreen.text("19.5", 5, 110);
+TFTscreen.text("19", 5, 120);
+TFTscreen.text("18.5", 5, 130);
+TFTscreen.text("18", 5, 140);
+TFTscreen.text("17.5", 5, 150);
+TFTscreen.line(30, 25, 30, 159);
+// Нарисовать график изменения освещённости
+for (int a = 35 ; a < 127 ; a++)
+{
+// Преобразовать освещённость в координату Y на экране ЖКИ
+q = map(sqmArray[a - 35],17.5,24,159,30);
+TFTscreen.point(a,q);
+}
+}
+void GraphPres()//график давления
+{
+  int q;
+// Вывести текущее давление
+//TFTscreen.background(0, 0, 0); // Очистить экран, установив черный цвет фона
+TFTscreen.stroke(0, 0, 255); 
+TFTscreen.setTextSize(1);
+TFTscreen.text("Pressure:", 3, 5);
+String presString=String(Pres);
+  //преобразовать измерение в символьный массив
+  presString.toCharArray(presstring,5);
+  TFTscreen.text("Pa", 100, 5);
+TFTscreen.text(presstring, 72, 5);
+// Нарисовать оси графика
+TFTscreen.setTextSize(1);
+TFTscreen.text("1085", 5, 20);
+TFTscreen.text("1067", 5, 30);
+TFTscreen.text("1049", 5, 40);
+TFTscreen.text("1031", 5, 50);
+TFTscreen.text("1013", 5, 60);
+TFTscreen.text("995", 5, 70);
+TFTscreen.text("977", 5, 80);
+TFTscreen.text("960", 5, 90);
+TFTscreen.text("942", 5, 100);
+TFTscreen.text("924", 5, 110);
+TFTscreen.text("906", 5, 120);
+TFTscreen.text("888", 5, 130);
+TFTscreen.text("870", 5, 140);
+TFTscreen.text("852", 5, 150);
+TFTscreen.line(35, 25, 35, 159);
+// Нарисовать график изменения освещённости
+for (int a = 40 ; a < 127 ; a++)
+{
+// Преобразовать освещённость в координату Y на экране ЖКИ
+q = map(presArray[a - 40],8520,10850,159,30);
+TFTscreen.point(a,q);
+}
+}
---
+++ SQM.ino
@@ -0,0 +1,208 @@
+//SQM - Монитор качества неба
+//полный код не влезает на ардуино нано, но можно убрать некоторые функции
+//подключаем библиотеки
+#include <Wire.h>//Подключение I2c
+#include <Adafruit_Sensor.h>//упрощает работу с сенсорами
+#include <math.h>//библиотека для вычислений
+#include <Adafruit_BME280.h>//датчик погоды(температура/давление/влажность)
+#include "Adafruit_TSL2591.h"//люксометр
+#include<TFT.h>//графический ЖКИ
+#include <EEPROM.h>//подключаем встроенное ПЗУ для записи калибовочных данных люксометра
+#include <SoftwareSerial.h>                       //  Подключаем библиотеку для работы с программным UART, до подключения библиотеки iarduino_GPS_NMEA.
+#include <iarduino_GPS_NMEA.h>                    //  Подключаем библиотеку для расшифровки строк протокола NMEA получаемых по UART.
+
+#define TFTcontrast 44//настройка вывода яркости экрана
+//настройка люксометра
+#define SQMBUTTON 7 //пин для кнопки включения люксометра
+#define LOWSCALE 1.0
+#define MEDSCALE 25.0
+#define HIGHSCALE 428.0
+#define MAXSCALE 9876.0
+#define SQM_CAL_OFFSET  -1.0  // обычная калибровка офсетами SQM из ЕЕПРОМ
+#define TEMP_CAL_OFFSET 0.0   // температурная калибровка
+                          
+
+//#define pinRX 6                          //  GPS Определяем вывод RX (программного UART) на плате Arduino к которому подключён вывод TX модуля. Номер вывода можно изменить.
+//#define pinTX 5  
+//время обновления
+#define UpdateMeteo 30
+#define UpdateGPS  20
+
+TFT TFTscreen = TFT(10,9,8);//настраиваем выводы ЖКЭ
+//SoftwareSerial    SerialGPS(pinRX, pinTX);        //  Объявляем объект SerialGPS для работы с функциями и методами библиотеки SoftwareSerial, указав выводы RX и TX Arduino.
+iarduino_GPS_NMEA gps;                            //  Объявляем объект gps для работы с функциями и методами библиотеки iarduino_GPS_NMEA.
+
+char tempstring[3];//для отображения числового значения переменной, её нужно преобразовать в символьный массив
+char presstring[5];
+char humstring[3];
+char luxstring[6];
+int tempArray[120];//массив для записи показаний датчика температуры
+double sqmArray[120];//массив для записи показаний датчика sqm
+float presArray[120];
+char currentString[3];
+volatile int Mode = 0;
+//присваиваем ключевое слово для вызова датчиков
+Adafruit_BME280 bme;
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+//переменные для хранения показаний датчиков
+double mag = 0;//SQM
+int Temp = 0;//метеоданные
+int Hum = 0;
+float Pres = 0;
+int hour;//gps
+char hourstring[3];
+int minute;
+char minutestring[3];
+int day=5;
+char daystring[3];
+int month=5;
+char monthstring[3];
+int year=24;
+char yearstring[5];
+double Lat;
+char latstring[8];
+double Long;
+char longstring[8];
+int Alt;
+char altstring[4];
+int SatAct;
+char satactstring[3];
+int SatVis;
+char satvisstring[3];
+//Луна
+
+int old;//возраст
+int moonNum=18;//лунное число
+//char moonstring[3];
+
+//Вспомогательные переменные для настройки датчиков
+double gainscale = MAXSCALE;
+uint32_t luminosity;
+uint16_t ir, full, visible;
+double adjustedVisible, adjustedIR;
+float SqmCalOffset =  SQM_CAL_OFFSET ;   // SQM калибровка из EEPROM
+float TempCalOffset = TEMP_CAL_OFFSET;   // температурная калибровка из EEPROM
+
+volatile uint32_t debounce;//это для функции прерывания если чо
+
+
+
+void setup() {
+  attachInterrupt(1, ModeFunc, CHANGE);
+  Serial.begin(9600);//ну так на всякий случай
+  Serial1.begin(9600);                       //  Инициируем работу с программной шиной UART для получения данных от GPS модуля на скорости 9600 бит/сек.
+     gps.begin(Serial1);                        //  Инициируем расшифровку строк NMEA указав объект используемой шины UART (вместо программной шины, можно указывать аппаратные: Serial, Serial1, Serial2, Serial3).
+     gps.timeZone(3);                             //  Указываем часовой пояс (±12 часов), или GPS_AutoDetectZone для автоматического определения часового пояса по долготе.
+   TFTscreen.begin();//активируем экран
+   TFTscreen.initR(INITR_GREENTAB);//Настройка экрана, сменить если есть смещение. На что? придётся погуглить
+  TFTscreen.background(0,0,0);//Очищаем(устанавливаем цвета rgb в 0, т.е. чёрный цвет)
+ TFTscreen.stroke(0,0,255);//установит цвет текста (красный)
+//настраиваем люксометр и датчик погоды
+pinMode(SQMBUTTON, INPUT);
+//pinMode(ModeButton, INPUT);
+bool status;
+status = bme.begin();
+tsl.begin();
+  tsl.setGain(TSL2591_GAIN_MAX);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);
+   SqmCalOffset = ReadEESqmCalOffset();     // Данные для калибровки люксометра из EEPROM
+   TempCalOffset = ReadEETempCalOffset();   // Калибровка температуры из EEPROM
+   startScreen();
+   ReadMeteo();
+   DisplMeteo();
+   ReadGPS();
+   ReadMoon();
+   DisplMoon();
+   DisplGPS();
+   ReadSQM();
+   DisplSQM();
+   if(mag>=20){
+   analogWrite(TFTcontrast,25);
+   }
+
+
+//   sqm.setCalibrationOffset(SqmCalOffset);  // хз пока, надо ли ваще
+}
+
+void loop() {
+
+switch (Mode){//функция переключения режимов работы
+  case 0:{//первый режим стартовый экран
+    startScreen();
+if (round(millis()/1000) % UpdateMeteo == 0)
+{
+  TFTscreen.stroke(0, 0, 0);//очистка предыдущих показаний
+  TFTscreen.text(tempstring,80,116);
+  TFTscreen.text(presstring,60,131);
+  TFTscreen.text(humstring,60,146);
+    ReadMeteo();
+    DisplMeteo(); //обновление погоды
+}
+if (digitalRead (SQMBUTTON) == HIGH)//показания люксометра снимаются по нажатии кнопки
+{
+  TFTscreen.stroke(0, 0, 0);
+   delay(100);//для устранения дребезга контактов
+  TFTscreen.fill(0,0,0);
+  TFTscreen.rect(55,20,70,20);
+   ReadSQM();
+   DisplSQM();
+}
+if (round(millis()/1000) % UpdateGPS == 0)//обновление gps
+{
+  TFTscreen.stroke(0, 0, 0);
+  TFTscreen.fill(0,0,0);
+  TFTscreen.rect(25,51,82,33);
+  /*
+  TFTscreen.setTextSize(3);
+  TFTscreen.text(hourstring,25,52);
+  TFTscreen.text(minutestring,73,52);
+  TFTscreen.setTextSize(1);
+  TFTscreen.text(daystring,38,75);
+  TFTscreen.text(monthstring,53,75);
+  TFTscreen.text(yearstring,68,75);
+  */
+  TFTscreen.text(satvisstring,5,69);
+  TFTscreen.text(satactstring,5,53);
+  TFTscreen.text(latstring,60,85);
+  TFTscreen.text(longstring,65,95);
+TFTscreen.text(altstring,65,105);
+    ReadGPS();
+    DisplGPS();
+  ReadMoon();
+  DisplMoon();
+}
+}
+break;
+case 1:{
+do {
+ ReadMeteo();
+  GraphTemp();
+}
+//GraphTemp();//проверить, может быть кривой
+while (round(millis()/1000) % UpdateMeteo == 0);
+}
+  break;
+  case 2:
+{
+  GraphPres();//проверить, может быть кривой
+if (round(millis()/1000) % UpdateMeteo == 0)
+  {
+  ReadMeteo();
+  GraphPres();
+  }
+  }
+  break;
+case 3:{
+  if (round(millis()/1000) % 5 == 0)
+  {
+  GraphSQM();//проверить
+  if (digitalRead (SQMBUTTON) == HIGH)
+  {
+    ReadSQM();
+   delay(100);
+  }
+  }
+  }
+}
+}
+   
